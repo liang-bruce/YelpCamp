8c7b216159f1967208d33ea28290680dc17ce4e0
Bruce-0602 <liangjitian777@gmail.com>
2022-07-26 14:50:26 +1000

    Fix Heroku error

--- app.js
+++ app.js
@@ -161,6 +161,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("HI, here is YelpCamp!");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
